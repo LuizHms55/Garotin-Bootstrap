4007cba65386afb88d17fb6c77501a29249d34c9
LuizHms55 <lhmsousa@gmail.com>
2025-05-26 16:29:02 -0300

    adição de imagens e cards que se expandem

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 ScrollReveal().reveal('.Membros', { delay: 500});
 ScrollReveal().reveal('.agenda', { delay: 100});
 ScrollReveal().reveal('.alb1', { delay: 100});
+
+const cards = document.querySelectorAll('.card-expandable');
+
+    cards.forEach(card => {
+      card.addEventListener('mouseenter', () => {
+        card.classList.add('expanded');
+      });
+
+      card.addEventListener('mouseleave', () => {
+        card.classList.remove('expanded');
+      });
+    });
