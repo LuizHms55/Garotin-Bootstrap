58be3bcf0371a86ac734c7f9bc8f316592414969
LuizHms55 <lhmsousa@gmail.com>
2025-06-02 22:37:01 -0300

    arrumando o index, adicionando animações e partir para as outras abas

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-ScrollReveal().reveal('.Membros', { delay: 500});
-ScrollReveal().reveal('.agenda', { delay: 100});
-ScrollReveal().reveal('.alb1', { delay: 100});
-
-const cards = document.querySelectorAll('.card-expandable');
-
+ const cards = document.querySelectorAll('.card-expandable');
     cards.forEach(card => {
-      card.addEventListener('mouseenter', () => {
-        card.classList.add('expanded');
-      });
-
-      card.addEventListener('mouseleave', () => {
-        card.classList.remove('expanded');
-      });
+      card.addEventListener('mouseenter', () => card.classList.add('expanded'));
+      card.addEventListener('mouseleave', () => card.classList.remove('expanded'));
     });
+
+ document.addEventListener("DOMContentLoaded", function () {
+  ScrollReveal().reveal('.reveal', {
+    origin: 'bottom',
+    distance: '40px',
+    duration: 400,
+    delay: 40,
+    easing: 'ease-in-out',
+    interval: 150,
+    reset: true,
+    viewFactor: 0.1
+  });
+});
+
