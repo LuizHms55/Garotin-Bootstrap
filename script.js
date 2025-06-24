9b4f254623095dd4877bcbb81174b3abacc1a8ff
LuizHms55 <lhmsousa@gmail.com>
2025-06-23 22:51:37 -0300

    final

--- script.js
+++ script.js
@@ -4,16 +4,24 @@
       card.addEventListener('mouseleave', () => card.classList.remove('expanded'));
     });
 
- document.addEventListener("DOMContentLoaded", function () {
-  ScrollReveal().reveal('.reveal', {
-    origin: 'bottom',
-    distance: '40px',
-    duration: 400,
-    delay: 40,
-    easing: 'ease-in-out',
-    interval: 150,
-    reset: true,
-    viewFactor: 0.1
+  // Hover nos cards expansíveis
+  document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll('.card-expandable');
+    cards.forEach(card => {
+      card.addEventListener('mouseenter', () => card.classList.add('expanded'));
+      card.addEventListener('mouseleave', () => card.classList.remove('expanded'));
+    });
+
+    // Scroll Reveal
+    ScrollReveal().reveal('.reveal', {
+      origin: 'bottom',
+      distance: '40px',
+      duration: 400,
+      delay: 40,
+      easing: 'ease-in-out',
+      interval: 150,
+      reset: true,
+      viewFactor: 0.1
+    });
   });
-});
 
